09f6e8ca0f63c9756d0bb4c5664108d1b1ab8280
jy3026 <jy_3026@naver.com>
2023-11-27 22:59:11 +0900

    ✨ [feat] 프로젝트 기본 세팅

---
+++ server-api/src/main/java/interview/bokka/api/BokkaApplication.java
@@ -0,0 +1,13 @@
+package interview.bokka.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokkaApplication.class, args);
+	}
+
+}
---
+++ server-domain/src/test/java/interview/bokka/BokkaApplicationTests.java
@@ -0,0 +1,13 @@
+package interview.bokka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
