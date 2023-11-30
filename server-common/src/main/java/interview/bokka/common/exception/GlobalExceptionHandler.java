a83161140d18216bb4306154d0e63cdadc9482d3
jy3026 <jy_3026@naver.com>
2023-11-30 22:36:54 +0900

    ✨ [feat] 전역 에러 헨들러 구현

---
+++ server-common/src/main/java/interview/bokka/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package interview.bokka.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ApplicationErrorResponse> handleException(Exception e) {
+        log.error("handleException {}", e.getMessage());
+
+        var response = new ApplicationErrorResponse(ApplicationErrorCode.INVALID_INPUT_VALUE.getCode(), ApplicationErrorCode.INVALID_INPUT_VALUE.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
