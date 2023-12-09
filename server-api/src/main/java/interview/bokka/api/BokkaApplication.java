23e4e0922860a48afb7b3930feba03ec06e5395b
jy3026 <jy_3026@naver.com>
2023-12-09 15:00:16 +0900

    ✨ [fix] Auditing 설정 변경

--- server-api/src/main/java/interview/bokka/api/BokkaApplication.java
+++ server-api/src/main/java/interview/bokka/api/BokkaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackageClasses = BasePackageLocation.class)
-@EnableJpaAuditing
 public class BokkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BokkaApplication.class, args);
