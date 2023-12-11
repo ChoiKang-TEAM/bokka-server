bf8eb6a09e1296c505cca4103829c3ecc27b01fa
jy3026 <jy_3026@naver.com>
2023-12-11 23:04:15 +0900

    ✨ [feat] Quiz Entity 추가

---
+++ server-domain/src/main/java/interview/bokka/domain/quiz/entity/Quiz.java
@@ -0,0 +1,35 @@
+package interview.bokka.domain.quiz.entity;
+
+import interview.bokka.common.auditing.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+public class Quiz extends BaseEntity {
+
+    @Id
+    @Column(name =  "quiz_id", nullable = false)
+    private Long id;
+
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @Column(name = "quiz_type", nullable = false)
+    private String quizType;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "answer", nullable = false)
+    private String answer;
+
+    @Column(name = "level", nullable = false)
+    private int level;
+}
