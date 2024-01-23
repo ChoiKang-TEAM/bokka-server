c9187326c8d3d8f23782a0d3f4aa30cb148f3df2
jy3026 <jy_3026@naver.com>
2024-01-23 16:11:56 +0900

    ✨ [feat] Swagger 설명 추가

--- server-api/src/main/java/interview/bokka/api/quiz/controller/QuizController.java
+++ server-api/src/main/java/interview/bokka/api/quiz/controller/QuizController.java
@@ -3,18 +3,21 @@ package interview.bokka.api.quiz.controller;
 import interview.bokka.api.quiz.service.QuizService;
 import interview.bokka.core.common.dto.ResponseDto;
 import interview.bokka.core.domain.quiz.dto.QuizSolveDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "퀴즈", description = "QUIZ API")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class QuizController {
     private final QuizService quizService;
 
+    @Operation(summary = "퀴즈 상세 조회", description = "주어진 퀴즈 ID에 해당하는 퀴즈를 상세히 조회합니다.")
     @GetMapping("/quizzes/{quizId}")
     public ResponseEntity<?> getQuiz(
             @PathVariable Long quizId
@@ -22,6 +25,7 @@ public class QuizController {
         return ResponseDto.ok(quizService.getDetail(quizId));
     }
 
+    @Operation(summary = "퀴즈 풀기", description = "주어진 퀴즈 ID에 해당하는 퀴즈를 정답을 받아 풉니다.")
     @PostMapping("/quizzes/{quizId}")
     public ResponseEntity<?> solveQuiz(
             @PathVariable Long quizId,
--- server-api/src/main/java/interview/bokka/api/user/controller/UserController.java
+++ server-api/src/main/java/interview/bokka/api/user/controller/UserController.java
@@ -5,6 +5,8 @@ import interview.bokka.core.common.consts.ResultCode;
 import interview.bokka.core.common.dto.ResponseDto;
 import interview.bokka.core.domain.user.dto.CreateUserReqDto;
 import interview.bokka.core.domain.user.dto.UserRespDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "유저", description = "USER API")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -19,6 +22,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "유저 생성", description = "유저 시퀸스를 받아 유저를 생성 합니다.")
     @PostMapping("/users/create")
     public ResponseEntity<?> createUser(@RequestBody CreateUserReqDto request) {
         userService.createUser(UserRespDto.toEntity(request));
