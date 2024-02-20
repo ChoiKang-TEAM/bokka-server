b11e2dc6e0d1ebe3d135af29d91d1e53207ae515
jy3026 <jy_3026@naver.com>
2024-02-20 22:03:03 +0900

    ✨ [fix] 결과값 추가

--- server-core/src/main/java/interview/bokka/core/common/consts/ResultCode.java
+++ server-core/src/main/java/interview/bokka/core/common/consts/ResultCode.java
@@ -1,8 +1,9 @@
 package interview.bokka.core.common.consts;
 
 public enum ResultCode {
-    SUCCESS(1000);
-
+    SUCCESS(1000),
+    EMPTY(1001),
+    BAD(2000);
     private final int value;
 
     ResultCode(int value) {
